388551f88f8b0be25053a0d4fc4c6f0f44739c4b
Sudhanvagowda <20cse0024@vvce.ac.in>
2021-02-16 11:18:15 +0000

    used int and char

--- Program_1.c
+++ Program_1.c
@@ -4,8 +4,8 @@ the word "TYPE" */
 #include<stdio.h>
 int main()
 {
-TYPE num = 100;
-TYPE letter = 'A';
+int num = 100;
+char letter = 'A';
 /* Printing num and charecter value */
 printf("%d\n",num);
 printf("%c\n",letter);
