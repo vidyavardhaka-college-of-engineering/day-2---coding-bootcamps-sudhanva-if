e3322df83f0fa07323738f4384e278ee6fb2d195
Sudhanvagowda <20cse0024@vvce.ac.in>
2021-02-16 12:02:42 +0000

    swaping of a and b takes place

--- Program_5.c
+++ Program_5.c
@@ -10,15 +10,18 @@
 int main()
 {
     int a, b, temp;
+    printf("enter the values for a and b\n");
     scanf("%d%d", &a, &b);
+    printf("the value of a and b before swaping is: %d%d\n",a,b);
 
     //Write your code here
     temp=a;
     a=b;
     b=temp;
     
-
+  printf("the values after swamping is: %d%d\n",a,b);
   
-    printf("%d %d\n", a, b);
+  
+   printf("%d %d\n", a, b);
     return 0;
 }
